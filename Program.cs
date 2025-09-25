a74f7dda2fe4004b931528e61e4a9563a0ee0410
Ängla Juliusson <angla02@icloud.com>
2025-09-25 10:02:57 +0200

    Slå ihop namn och meddelande och skriv ut i konsollen

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ namespace myGuestBook {
         Console.WriteLine("Fel: Inget namn angivet.");
         return; // Om felkod stoppa koden
         }
-        
+
         // Fråga efter meddelande
         Console.WriteLine("Skriv något om ditt besök:");
         var message = Console.ReadLine();
@@ -36,6 +36,12 @@ namespace myGuestBook {
         Console.WriteLine("Fel: Inget meddelande angivet.");
         return; // Om felkod stoppa koden
         }
+
+        // Slå ihop namn och meddelande och skriv ut det i konsollen
+        var newGuest = new Guest(name, message);
+        Console.WriteLine(); // Tom rad
+        Console.WriteLine($"Tack för ditt besök {newGuest.guest}, ditt meddelande är sparat:");
+        Console.WriteLine(newGuest.message);
     }
     }
 }
