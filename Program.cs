fd9316e98f82461783f121817253b226fce275ea
Ängla Juliusson <angla02@icloud.com>
2025-09-25 11:08:59 +0200

    if-sats för alternativen. Flytta utskriften för nytt inlägg in i alternativ 1

--- Program.cs
+++ Program.cs
@@ -30,35 +30,48 @@ namespace myGuestBook {
 
         var choice = Console.ReadLine(); // Val av alternativ
 
-        if (choice == "4") break; // Avsluta program
-        // Annars kör funktion beroende på val
+        // Alternativ 1 - Lägg till inlägg
+        if (choice == "1") {
+
+            // Fråga efter namn
+            Console.WriteLine("Ange ditt namn:");
+            var name = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(name)) // Kontrollerar att användaren skrivit något
+            {
+            Console.WriteLine("Fel: Inget namn angivet.");
+            return; // Om felkod stoppa koden
+            }
+
+            // Fråga efter meddelande
+            Console.WriteLine("Skriv något om ditt besök:");
+            var message = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(message)) // Kontrollerar att användaren skrivit något
+            {
+            Console.WriteLine("Fel: Inget meddelande angivet.");
+            return; // Om felkod stoppa koden
+            }
+
+            // Slå ihop namn och meddelande och skriv ut det i konsollen
+            var newGuest = new Guest(name, message);
+            Console.WriteLine(); // Tom rad
+            Console.WriteLine($"Tack för ditt besök {newGuest.guest}, ditt meddelande är sparat:");
+            Console.WriteLine(newGuest.message);
+        
+        }
+        // Alternativ 2 - Ta bort inlägg
+        if (choice == "2") {
+
+        // Alternativ 3 - Visa alla inlägg
+        } 
+        if (choice == "3") {
+
+        } 
+        // Alternativ 4 - Avsluta program
+        if (choice == "4") break;
+    }
+
+    }
 }
-
-        // Fråga efter namn
-        Console.WriteLine("Ange ditt namn:");
-        var name = Console.ReadLine();
-
-        if (string.IsNullOrWhiteSpace(name)) // Kontrollerar att användaren skrivit något
-        {
-        Console.WriteLine("Fel: Inget namn angivet.");
-        return; // Om felkod stoppa koden
-        }
-
-        // Fråga efter meddelande
-        Console.WriteLine("Skriv något om ditt besök:");
-        var message = Console.ReadLine();
-
-        if (string.IsNullOrWhiteSpace(message)) // Kontrollerar att användaren skrivit något
-        {
-        Console.WriteLine("Fel: Inget meddelande angivet.");
-        return; // Om felkod stoppa koden
-        }
-
-        // Slå ihop namn och meddelande och skriv ut det i konsollen
-        var newGuest = new Guest(name, message);
-        Console.WriteLine(); // Tom rad
-        Console.WriteLine($"Tack för ditt besök {newGuest.guest}, ditt meddelande är sparat:");
-        Console.WriteLine(newGuest.message);
-    }
-    }
 }
