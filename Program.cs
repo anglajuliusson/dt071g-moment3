2c1a43a1488556eaaf6b37cc80a0b00e96c06d56
Ängla Juliusson <angla02@icloud.com>
2025-09-25 09:47:38 +0200

    Slå ihop namnet och meddelandet på gästen

--- Program.cs
+++ Program.cs
@@ -5,7 +5,14 @@ using System.Net.Http.Headers;
 namespace myGuestBook {
     
     class Guest {
-        
+        public string guest; // Gästen
+        public string message; // Meddelandet
+
+        public Guest(string guest, string message) // Slå ihop namn med meddelandet
+        {
+            this.guest = guest; // Spara gästens namn i fältet
+            this.message = message; // Spara meddelandet i fältet
+        }
     }
     class GuestBook {
 
